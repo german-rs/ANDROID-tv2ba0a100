65e058f2af3d12137eacbfa60bafe971baa37e0a
Germán Riveros <germanriveros@MacBook-Pro-de-German.local>
2016-05-12 15:40:04 -0300

    primer commit posterior a la creacion del proyecto

---
+++ app/src/androidTest/java/com/example/germanriveros/android_tv2ba0a100/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.germanriveros.android_tv2ba0a100;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/germanriveros/android_tv2ba0a100/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.germanriveros.android_tv2ba0a100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
