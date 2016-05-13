abf7b2044468088ddb0342cf91e084ce2ee78fe3
Germán Riveros <germanriveros@MacBook-Pro-de-German.local>
2016-05-13 00:45:46 -0300

    Object ContentResolver

--- app/src/main/java/com/example/germanriveros/android_tv2ba0a100/MainActivity.java
+++ app/src/main/java/com/example/germanriveros/android_tv2ba0a100/MainActivity.java
@@ -1,13 +1,107 @@
 package com.example.germanriveros.android_tv2ba0a100;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
+    private ContentResolver contentResolver;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Log.d("Ejercutando_metodo: ", "onCreate");
+
+        //Intent i = new Intent(MainActivity.this, SecondActivity.class);
+       // i.putExtra("variable","dato");
+        //startActivity(i);
+
+
+        access();
     }
+
+
+    public void access()
+    {
+        contentResolver = getContentResolver();
+        Cursor cur = contentResolver.query(Contacts.People.CONTENT_URI,
+                null, null, null,null);
+
+
+        if(cur.getCount() > 0)
+
+            while(cur.moveToNext())
+            {
+                String id = cur.getString(cur.getColumnIndex(Contacts.People._ID));
+                String name= cur.getString(cur.getColumnIndex(Contacts.People.DISPLAY_NAME));
+
+                Log.d("ContentProvider",name);
+                Log.d("ContentProvider", id);
+
+
+            }
+
+
+
+    }//..
+
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("Ejercutando_metodo: ", "onStart");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("Ejercutando_metodo: ", "onResume");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("Ejercutando_metodo: ", "onPause");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("Ejercutando_metodo: ", "onRestar");
+    }
+
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("Ejercutando_metodo: ", "onStop");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("Ejercutando_metodo: ", "onDestroy");
+    }
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/germanriveros/android_tv2ba0a100/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.germanriveros.android_tv2ba0a100;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by germanriveros on 12-05-16.
+ */
+public class SecondActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle saveInstanceState)
+    {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //String varString = getIntent().getStringExtra("variable");
+        //Log.d("SecondActivity: ", varString);
+
+    }
+
+}
