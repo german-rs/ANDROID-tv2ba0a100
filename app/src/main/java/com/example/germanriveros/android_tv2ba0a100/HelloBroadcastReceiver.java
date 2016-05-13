0c98f94e5eb151f5bd7d869511253cfd370b25ab
Germán Riveros <germanriveros@MacBook-Pro-de-German.local>
2016-05-13 17:05:29 -0300

    BroadcastReceiver

---
+++ app/src/main/java/com/example/germanriveros/android_tv2ba0a100/HelloBroadcastReceiver.java
@@ -0,0 +1,21 @@
+package com.example.germanriveros.android_tv2ba0a100;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by germanriveros on 13-05-16.
+ */
+public class HelloBroadcastReceiver extends BroadcastReceiver
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+        Log.d("BroadcastReceiver","power is connected!");
+
+    }
+}
--- app/src/main/java/com/example/germanriveros/android_tv2ba0a100/MainActivity.java
+++ app/src/main/java/com/example/germanriveros/android_tv2ba0a100/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.germanriveros.android_tv2ba0a100;
 
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
 import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
@@ -13,46 +16,21 @@ public class MainActivity extends AppCompatActivity
 {
 
     private ContentResolver contentResolver;
+    private BroadcastReceiver mReceiver;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Log.d("Ejercutando_metodo: ", "onCreate");
-
         //Intent i = new Intent(MainActivity.this, SecondActivity.class);
        // i.putExtra("variable","dato");
         //startActivity(i);
+        //access();
 
-
-        access();
     }
-
-
-    public void access()
-    {
-        contentResolver = getContentResolver();
-        Cursor cur = contentResolver.query(Contacts.People.CONTENT_URI,
-                null, null, null,null);
-
-
-        if(cur.getCount() > 0)
-
-            while(cur.moveToNext())
-            {
-                String id = cur.getString(cur.getColumnIndex(Contacts.People._ID));
-                String name= cur.getString(cur.getColumnIndex(Contacts.People.DISPLAY_NAME));
-
-                Log.d("ContentProvider",name);
-                Log.d("ContentProvider", id);
-
-
-            }
-
-
-
-    }//..
-
 
 
     @Override
@@ -65,6 +43,10 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume()
     {
+
+        this.mReceiver = new HelloBroadcastReceiver();
+        registerReceiver(this.mReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
         super.onResume();
         Log.d("Ejercutando_metodo: ", "onResume");
     }
@@ -72,6 +54,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onPause()
     {
+
+        unregisterReceiver(mReceiver);
+
         super.onPause();
         Log.d("Ejercutando_metodo: ", "onPause");
     }
@@ -100,6 +85,31 @@ public class MainActivity extends AppCompatActivity
 
 
 
+    public void access()
+    {
+        //Este ejemplo no funciono con el simulador
+
+        contentResolver = getContentResolver();
+        Cursor cur = contentResolver.query(Contacts.People.CONTENT_URI,
+                null, null, null,null);
+
+
+        if(cur.getCount() > 0)
+
+            while(cur.moveToNext())
+            {
+                String id = cur.getString(cur.getColumnIndex(Contacts.People._ID));
+                String name= cur.getString(cur.getColumnIndex(Contacts.People.DISPLAY_NAME));
+
+                Log.d("ContentProvider",name);
+                Log.d("ContentProvider", id);
+
+
+            }
+
+
+
+    }//..
 
 
 
